ba8c6db00b47ca969cd9e893d8951d25d8c7228c
Shota Igo <igoshota@gmail.com>
2018-04-22 01:27:15 +0000

    motor 8 shou

--- scripts/buzzer4.py
+++ scripts/buzzer4.py
@@ -3,7 +3,6 @@
 import rospy, actionlib
 from std_msgs.msg import UInt16
 from pi_ros.msg import MusicAction, MusicResult, MusicFeedback # 行を追加
-
 def write_freq(hz=0):
     bfile = "/dev/rtbuzzer0"
     try:
@@ -11,21 +10,7 @@ def write_freq(hz=0):
             f.write(str(hz) + "\n")
     except IOError:
         rospy.logerr("can't write to " + bfile)
-def exec_music(goal):
-    r = MusicResult()
-    fb = MusicFeedback()
-    for i, f in enumerate(goal.freqs):
-        fb.remaining_steps = len(goal.freqs) - i
-        music.publish_feedback(fb)
-        if music.is_preempt_requested():
-            write_freq(0)
-            r.finished = False
-            music.set_preempted(r)
-            return
-        write_freq(f)
-        rospy.sleep(1.0 if i >= len(goal.durations) else goal.durations[i])
-    r.finished = True
-    music.set_succeeded(r)
+def exec_music(goal): pass        # 追加
 def recv_buzzer(data):
     write_freq(data.data)
 if __name__ == '__main__':
